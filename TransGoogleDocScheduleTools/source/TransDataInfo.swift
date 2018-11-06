4c791f3f03324e2d5fb898af632963e7cc1d8d69
esp <esp@gosmio.biz>
2018-11-06 22:23:10 +0800

    [style] [Gadgets] [TransGoogleDocScheduleTools]
    # TransGoogleDocScheduleTools
    *  排版
     * 註解說明

--- TransGoogleDocScheduleTools/source/TransDataInfo.swift
+++ TransGoogleDocScheduleTools/source/TransDataInfo.swift
@@ -9,35 +9,35 @@
 import Cocoa
 
 /**
- * @brief 轉換google execl 的資訊.
+ * @brief google execl 自動 mapping 另一個 doc 欄位的資訊。
  * @details - 目前是很特例化的應用情境，只是想先減少人為的工。
+ *            並無加上太多防呆，請謹慎使用。
+ *            此為從某個 google excel 連結到另一個 google excel 欄位。
  */
 class TransDataInfo: NSObject {
-    
     /** google doc url */
-    let srcURL:String!
-    
+    let srcURL: String!
+
     /** google doc page name */
-    let srcPageName:String!
-    
+    let srcPageName: String!
+
     /** google doc column name */
-    let srcColumnName:String!
-    
+    let srcColumnName: String!
+
     /** 起始的數字 */
-    let fromNum:Int
-    
+    let fromNum: Int
+
     /** for 迴圈處理次數 */
-    let dealCount:Int
-    
+    let dealCount: Int
+
     /** 每次需疊加的數字 */
-    let accumulateNum:Int
-    
+    let accumulateNum: Int
+
     /** 需額外增加的空白行 */
-    let extraEmptyRowNum:Int
-    
-    init(_ srcURL:String, _ srcPageName:String, _ srcColumnName:String,
-         _ fromNum:Int, _ dealCount:Int, _ accumulateNum:Int, _ extraEmptyRowNum:Int)
-    {
+    let extraEmptyRowNum: Int
+
+    init(_ srcURL: String, _ srcPageName: String, _ srcColumnName: String,
+         _ fromNum: Int, _ dealCount: Int, _ accumulateNum: Int, _ extraEmptyRowNum: Int) {
         self.srcURL = srcURL
         self.srcPageName = srcPageName
         self.srcColumnName = srcColumnName
@@ -46,29 +46,32 @@ class TransDataInfo: NSObject {
         self.accumulateNum = accumulateNum
         self.extraEmptyRowNum = extraEmptyRowNum
     }
-    
-    func excuteTransInfo() -> NSArray! {
-        
-        let outputLogs:NSMutableArray! = NSMutableArray()
-        
+
+    /** 自動產生 mapping google excel 欄位資料 */
+    func generatorTransInfo() -> NSArray! {
+        let outputLogs: NSMutableArray! = NSMutableArray()
+
         // 處理轉換字串的輸出內容.
-        for count in 0 ..< self.dealCount
-        {
-            let tempLog:NSString = NSString.init(format:"=IMPORTRANGE(\"%@\", \"%@!%@%d\")",
-                                                 self.srcURL, self.srcPageName, self.srcColumnName,
-                                                 self.fromNum + count * self.accumulateNum)
+        for count in 0 ..< dealCount {
             
+            // sample code
+            // =IMPORTRANGE("https://docs.google.com/spreadsheets/d/1IDwIdGm9hBOLFWQ2JV39BJD3DwcODdhFHlf5RMU9itw/edit#gid=0","2018!E410")
+            
+            // 處理通用規則.
+            let tempLog: NSString = NSString(format: "=IMPORTRANGE(\"%@\", \"%@!%@%d\")",
+                                             srcURL, srcPageName, srcColumnName,
+                                             fromNum + count * accumulateNum)
+
             outputLogs.add(tempLog)
-            
-            if self.extraEmptyRowNum > 0
-            {
-                for _ in 0 ..< self.extraEmptyRowNum
-                {
+
+            // 額外處理是否需新增空白行.
+            if extraEmptyRowNum > 0 {
+                for _ in 0 ..< extraEmptyRowNum {
                     outputLogs.add("")
                 }
             }
         }
-        
+
         return outputLogs.copy() as? NSArray
     }
 }
