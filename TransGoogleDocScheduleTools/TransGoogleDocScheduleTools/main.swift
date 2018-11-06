046c2998533429a4671553d222ab0203046eff7c
esp <esp@gosmio.biz>
2018-11-06 21:57:56 +0800

    [feature] [Gadgets] [TransGoogleDocScheduleTools] [新增 剖析每次處理需額外加空白行功能]
    # TransGoogleDocScheduleTools
    
    * 新增 剖析每次處理需額外加空白行功能
    * 實際處理功能拉到 TransDataInfo 類別處理

--- TransGoogleDocScheduleTools/TransGoogleDocScheduleTools/main.swift
+++ TransGoogleDocScheduleTools/TransGoogleDocScheduleTools/main.swift
@@ -12,8 +12,6 @@ import Foundation
 // Test Code，之後可以研究 main arguments 使用方式。
 //let elements = CommandLine.arguments
 
-
-
 /*
  * @brief - google excel 連結欄位產生器.
  * @details - 此為從某個 google excel 連結到另一個 google excel 欄位.
@@ -24,35 +22,45 @@ import Foundation
 print("Initial TransDataInfo!")
 
 // 暫時寫死，之後可能改成讀取檔案方式.
-let srcURL:String! = "https://docs.google.com/spreadsheets/d/1IDwIdGm9hBOLFWQ2JV39BJD3DwcODdhFHlf5RMU9itw/edit#gid=0"
-let srcPageName:String! = "2018!E"
-let fromNum:Int = 428
+//let srcURL:String! = "https://docs.google.com/spreadsheets/d/1IDwIdGm9hBOLFWQ2JV39BJD3DwcODdhFHlf5RMU9itw/edit#gid=0"
+//let srcPageName:String! = "2018"
+//let srcColumnName:String! = "E"
+//let fromNum:Int = 428
+//let dealCount:Int = 100
+//let accumulateNum:Int = 3
+//let outputFileName:String! = "Desktop/tempTransFile.txt"
+//let extraEmptyRowNum:Int = 0
+
+// 暫時寫死，之後可能改成讀取檔案方式.
+let srcURL:String! = "https://docs.google.com/spreadsheets/d/1EqJiTQy-6VSTkfuSAzJqK1hrx8WAnegj-FnejNEIamI/edit?pli=1#gid=713291970"
+let srcPageName:String! = "工作排程"
+let srcColumnName:String! = "S"
+let fromNum:Int = 3
 let dealCount:Int = 100
-let accumulateNub:Int = 3
-let outputFileName:String! = "Desktop/tempTransFile.txt"
+let accumulateNum:Int = 1
+let outputFileName:String! = "Desktop/tempTransFile_mou.txt"
+let extraEmptyRowNum:Int = 2
 
-var dataInfo:TransDataInfo = TransDataInfo(srcURL, srcPageName,
-                                           fromNum, dealCount, accumulateNub)
+print("create TransDataInfo")
+var dataInfo:TransDataInfo = TransDataInfo(srcURL, srcPageName, srcColumnName,
+                                           fromNum, dealCount, accumulateNum, extraEmptyRowNum)
 
-var outputLogs:NSMutableArray! = NSMutableArray()
+print("deal TransDataInfo trans log")
+
+var outputLogs:NSArray! = dataInfo.excuteTransInfo()
 
 let homeDirURL:URL = URL(fileURLWithPath: NSHomeDirectory())
 
 let pathURL:URL = homeDirURL.appendingPathComponent(outputFileName)
 let path = homeDirURL.absoluteString + outputFileName
 
-// 處理轉換字串的輸出內容.
-for count in 1...dataInfo.dealCount
-{
-    let tempLog:NSString = NSString.init(format: "=IMPORTRANGE(\"%@\", \"%@%d\")", dataInfo.srcURL
-        , dataInfo.srcPageName, dataInfo.fromNum + count * accumulateNub)
-    
-    outputLogs.add(tempLog)
-}
-
 // 處理寫檔.
 let arrayToSave = NSArray(array: outputLogs)
 
 let outputLog:String = outputLogs.componentsJoined(by: "\r\n")
+
+
+print("write outputLog to file: ", pathURL)
 try outputLog.write(to: pathURL, atomically: true, encoding: String.Encoding.utf8)
 
+print("TransGoogleDocScheduleTools excute finish")
--- TransGoogleDocScheduleTools/source/TransDataInfo.swift
+++ TransGoogleDocScheduleTools/source/TransDataInfo.swift
@@ -11,21 +11,64 @@ import Cocoa
 /**
  * @brief 轉換google execl 的資訊.
  * @details - 目前是很特例化的應用情境，只是想先減少人為的工。
-  */
+ */
 class TransDataInfo: NSObject {
-    let srcURL:String!
-    let srcPageName:String!
-    let fromNum:Int
-    let dealCount:Int
-    let accumulateNub:Int
     
-    init(_ srcURL:String, _ srcPageName:String, _ fromNum:Int,
-         _ dealCount:Int, _ accumulateNub:Int)
+    /** google doc url */
+    let srcURL:String!
+    
+    /** google doc page name */
+    let srcPageName:String!
+    
+    /** google doc column name */
+    let srcColumnName:String!
+    
+    /** 起始的數字 */
+    let fromNum:Int
+    
+    /** for 迴圈處理次數 */
+    let dealCount:Int
+    
+    /** 每次需疊加的數字 */
+    let accumulateNum:Int
+    
+    /** 需額外增加的空白行 */
+    let extraEmptyRowNum:Int
+    
+    init(_ srcURL:String, _ srcPageName:String, _ srcColumnName:String,
+         _ fromNum:Int, _ dealCount:Int, _ accumulateNum:Int, _ extraEmptyRowNum:Int)
     {
         self.srcURL = srcURL
         self.srcPageName = srcPageName
+        self.srcColumnName = srcColumnName
         self.fromNum = fromNum
         self.dealCount = dealCount
-        self.accumulateNub = accumulateNub
+        self.accumulateNum = accumulateNum
+        self.extraEmptyRowNum = extraEmptyRowNum
+    }
+    
+    func excuteTransInfo() -> NSArray! {
+        
+        let outputLogs:NSMutableArray! = NSMutableArray()
+        
+        // 處理轉換字串的輸出內容.
+        for count in 0 ..< self.dealCount
+        {
+            let tempLog:NSString = NSString.init(format:"=IMPORTRANGE(\"%@\", \"%@!%@%d\")",
+                                                 self.srcURL, self.srcPageName, self.srcColumnName,
+                                                 self.fromNum + count * self.accumulateNum)
+            
+            outputLogs.add(tempLog)
+            
+            if self.extraEmptyRowNum > 0
+            {
+                for _ in 0 ..< self.extraEmptyRowNum
+                {
+                    outputLogs.add("")
+                }
+            }
+        }
+        
+        return outputLogs.copy() as? NSArray
     }
 }
